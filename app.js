eb32ca515e4437b59240c4a96ae01ec56ca72169
Rahul <rahulam19aug@gmail.com>
2025-09-17 17:29:06 +0530

    Add project Files

--- app.js
+++ app.js
@@ -101,7 +101,7 @@ app.use((req,res,next)=>{
 
 
 app.use("/listings", listingRouter);
-app.use("/listins/:id/reviews", reviewsRouter);
+app.use("/listings/:id/reviews", reviewsRouter);
 app.use("/",userRouter);
 
 app.all(/.*/,(req,res,next)=>{
